d6f788f9dba61ae5ff7922479f31d61a21d913ae
hoodiesn <sitplekda@gmail.com>
2023-06-01 07:57:06 +0300

    ..

--- s21_ceil.c
+++
@@ -1,9 +0,0 @@
-#include "s21_math.h"
-// Возвращает ближайшее целое число возвращает ближайшее целое число, не
-// превышающее заданное значение
-long double s21_ceil(double x) {
-  return (x == (long long int)x) ? x
-         : (x < 0)               ? (long double)(((long long int)x))
-         : (x > 0)               ? (long double)((long long int)x) + 1
-                                 : 0;
-}
--- s21_fabs.c
+++
@@ -1,3 +0,0 @@
-#include "s21_math.h"
-// модуль числа с плавающей точкой
-long double s21_fabs(double x) { return x < 0 ? x * -1 : x; }
